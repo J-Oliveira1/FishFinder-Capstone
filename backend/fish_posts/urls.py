1ff8edf6a310ca8fd7afa30425cef7f8fa865d2b
unknown <j.poliveira90@outlook.com>
2023-03-09 12:21:56 -0500

    Added and tested both Put and Delete request for Fish_Posts App.

--- backend/fish_posts/urls.py
+++ backend/fish_posts/urls.py
@@ -4,4 +4,6 @@ from fish_posts import views
 urlpatterns = [
     path('new/', views.fishing_posts),
     path('', views.get_fish_posts),
+    path('<int:fish_posts_id>/update/', views.update_fish_posts),
+    path('<int:fish_posts_id>/delete/', views.delete_fish_post),
 ]
--- backend/fish_posts/views.py
+++ backend/fish_posts/views.py
@@ -20,12 +20,30 @@ def get_fish_posts(request, fishing_hole_id):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def fishing_posts(request, fishing_hole_id):
-    print(
-        'User ', f"{request.user.id} {request.user.email} {request.user.username}"
-    )
     if request.method == 'POST':
         serializer = FishPostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user, fishing_hole_id=fishing_hole_id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)       
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_fish_posts(request, fishing_hole_id, fish_posts_id):
+    fish_posts = get_object_or_404(FishPost, pk=fish_posts_id, fishing_hole_id=fishing_hole_id)
+    if request.method == 'PUT':
+        serializer = FishPostSerializer(fish_posts, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user, fishing_hole_id=fishing_hole_id, fish_posts=fish_posts)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_fish_post(request, fish_posts_id, fishing_hole_id):
+    fish_posts = get_object_or_404(FishPost, pk=fish_posts_id, fishing_hole_id=fishing_hole_id)
+    fish_posts.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+          
