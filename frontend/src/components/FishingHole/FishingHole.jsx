e2de07bea6fbfbac576bf32c54c7f9fd7d29abf6
unknown <j.poliveira90@outlook.com>
2023-04-05 17:38:18 -0400

    Fixed issue with rounding out the star rating to round up to one decimal place.

--- frontend/src/components/FishingHole/FishingHole.jsx
+++ frontend/src/components/FishingHole/FishingHole.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../FishingHole/FishingHole.css"
+import { ceil, round } from 'math';
+
 
 const FishingHole = ({ fishingHole, fishingHoleId }) => {
   const navigate = useNavigate();
@@ -16,7 +18,7 @@ const FishingHole = ({ fishingHole, fishingHoleId }) => {
       <div onClick={handleClick} >
         <h4>Username: {fishingHole.username}</h4>
         <p>Fishing Hole ID: {fishingHoleId}</p>
-        <p>Average Rating: {fishingHole.average_rating || 0} <span className="star">&#9733;</span></p>
+        <p>Average Rating: {fishingHole.average_rating ? fishingHole.average_rating.toFixed(1) : 0} <span className="star">&#9733;</span></p>
         <p>Address: {fishingHole.address}</p>
         <p>Latitude: {fishingHole.latitude}</p>
         <p>Longitude: {fishingHole.longitude}</p>
